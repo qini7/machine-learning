2a8094c1b7c998e1bf0d0cc3b988fbf90fe2b13e
qini7 <664073217@qq.com>
2017-07-27 16:58:52 +0800

    ex4_clear

--- machine-learning-ex1/ex1/testFile.m
+++
@@ -1,8 +0,0 @@
-
-data = load('ex1data1.txt');
-X = data(:, 1); y = data(:, 2);
-m = length(y); % number of training examples
-plot(X,y, 'rx', 'MarkerSize', 10);
-xlabel('x')
-ylabel('y')
-title('test')
